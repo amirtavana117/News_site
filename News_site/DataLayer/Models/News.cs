be3b6eddb15cea07151c2ec170e8197d25ae24ca
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-08 11:39:29 -0800

    Complete News Model

--- News_site/DataLayer/Models/News.cs
+++ News_site/DataLayer/Models/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,33 @@ namespace DataLayer
 {
     public class News
     {
+        [Key]
+        public int NewsId { get; set; }
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public int GroupId { get; set; }
+        [Display(Name = "عنوان خبر")]
+        [MaxLength(150)]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public string NewsTitle { get; set; }
+        [Display(Name = "توضیح مختصر")]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        [DataType(DataType.MultilineText)]
+        [MaxLength(350)]
+        public string ShortDescribtion { get; set; }
+        [Display(Name = "خبر")]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        [DataType(DataType.MultilineText)]
+
+        public string NewsText { get; set; }
+        [Display(Name = "بازدید")]
+        public int visited { get; set; }
+        [Display(Name = "نام تصویر")]
+        public string ImageName { get; set; }
+        [Display(Name = "اسلایدر")]
+        public bool ShoWInslider { get; set; }
+        [Display(Name = "زمان ایجاد")]
+        public DateTime CreateDate { get; set; }
     }
+
 }
