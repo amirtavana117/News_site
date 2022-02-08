c014db968244184bfeb6fe8ec81c94babf5df3fa
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-08 11:53:09 -0800

    Complete NewComment Model

--- News_site/DataLayer/Models/NewsComment.cs
+++ News_site/DataLayer/Models/NewsComment.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer
 {
-    public class  NewsComment
+    public class NewsComment
     {
+        [Key]
+        public int CommentId { get; set; }
+        [Display(Name = "خبر")]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public int NewsId { get; set; }
+        [Display(Name = "150")]
+        [MaxLength()]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public string FullName { get; set; }
+        [Display(Name = "ایمیل")]
+        [MaxLength(150)]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public String Email { get; set; }
+        [Display(Name = "نظر")]
+        [MaxLength(350)]
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public string Comment { get; set; }
+        [Display(Name = "زمان ایجاد")]
+        public DateTime CreateDate { get; set; }
     }
 }
