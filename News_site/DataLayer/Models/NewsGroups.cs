986c7e441871bc6b4a6cb2109b9a14c5ef3c909c
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-08 11:32:49 -0800

    Complete NewsGroups Model

--- News_site/DataLayer/Models/NewsGroups.cs
+++ News_site/DataLayer/Models/NewsGroups.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace DataLayer
 {
     public class NewsGroups
     {
+        [Key]
+        public int GroupId { get; set; }
+        [Display(Name ="عنوان خبر")]
+        [MaxLength(30)]
+        [Required(ErrorMessage = "وارد کنید{0} لطفا ")]
+        public string GroupTitle { get; set; }
     }
 }
