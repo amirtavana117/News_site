62c381caf04b87e6c073622e5368c7643e90c49b
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-08 11:58:22 -0800

    Add Context

---
+++ News_site/DataLayer/Context/NewsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class NewsContext :DbContext
+    {
+        public DbSet<News> news { get; set; }
+        public DbSet<NewsComment> newsComments { get; set; }
+        public DbSet<NewsGroups> newsGroups { get; set; }
+    }
+}
