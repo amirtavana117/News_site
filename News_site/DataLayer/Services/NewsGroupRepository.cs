4c1593f6d44a0ec6a77ad37275bc1b4cc2720ba5
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-09 04:46:34 -0800

    Add NewsGroup  Services

---
+++ News_site/DataLayer/Services/NewsGroupRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class NewsGroupRepository : INewsGroupRepository
+    {
+
+        NewsContext db = new NewsContext();
+        public IEnumerable<NewsGroups> GetAllNewsGroups()
+        {
+            return db.newsGroups;
+        }
+
+        public NewsGroups GetNewsById(int id)
+        {
+            return db.newsGroups.Find(id);
+        }
+
+        public bool insertNewsGroup(NewsGroups newsGroups)
+        {
+            try
+            {
+                db.newsGroups.Add(newsGroups);
+                return true;
+            }
+            catch (Exception)
+            {
+
+               return false;
+            }
+        }
+        public bool updateNewsGroup(NewsGroups newsGroups)
+        {
+            try
+            {
+                db.Entry(newsGroups).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public bool DeleteNewsGroup(NewsGroups newsGroups)
+        {
+            try
+            {
+                db.Entry(newsGroups).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool DeleteNewsGroup(int newsGroupId)
+        {
+            try
+            {
+                var newsGroup = GetNewsById(newsGroupId);
+                DeleteNewsGroup(newsGroup);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public void save()
+        {
+            db.SaveChanges();
+        }
+
+
+    }
+}
