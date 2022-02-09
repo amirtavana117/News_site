639d22c3fb54b64202e1d2b9956205605a11acfd
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-09 03:22:47 -0800

    Add NewsGroup Repository

---
+++ News_site/DataLayer/Repository/INewsGroupRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public interface INewsGroupRepository :IDisposable
+    {
+        IEnumerable<NewsGroups> GetAllNewsGroups();
+        NewsGroups GetNewsById(int id);
+        bool insertNewsGroup(NewsGroups newsGroups);
+        bool updateNewsGroup(NewsGroups newsGroups);
+        bool DeleteNewsGroup(NewsGroups newsGroups);
+        bool DeleteNewsGroup(int newsGroupId);
+        void save();
+
+    }
+}
