976bc5adc1c2219233521c41f44f6def500d6cb0
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-09 23:55:16 -0800

    Add NewsContoroller

---
+++ News_site/News_site/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace News_site.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private NewsContext db = new NewsContext();
+
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var news = db.news.Include(n => n.newsGroups);
+            return View(news.ToList());
+        }
+
+        // GET: Admin/News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: Admin/News/Create
+        public ActionResult Create()
+        {
+            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle");
+            return View();
+        }
+
+        // POST: Admin/News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NewsId,GroupId,NewsTitle,ShortDescribtion,NewsText,visited,ImageName,ShoWInslider,CreateDate")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.news.Add(news);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            return View(news);
+        }
+
+        // GET: Admin/News/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            return View(news);
+        }
+
+        // POST: Admin/News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NewsId,GroupId,NewsTitle,ShortDescribtion,NewsText,visited,ImageName,ShoWInslider,CreateDate")] News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            return View(news);
+        }
+
+        // GET: Admin/News/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.news.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: Admin/News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            News news = db.news.Find(id);
+            db.news.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
