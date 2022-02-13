a3ee1c385729177cd61ade1801ea8fad0959d1e0
Amir mahdi Tavana <amirredfox117@gmail.com>
2022-02-13 10:41:35 -0800

    change News Controller . Add News and Newsgroup Repository

--- News_site/News_site/Areas/Admin/Controllers/NewsController.cs
+++ News_site/News_site/Areas/Admin/Controllers/NewsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -12,13 +13,20 @@ namespace News_site.Areas.Admin.Controllers
 {
     public class NewsController : Controller
     {
+        private INewsRepository newsRepository;
+        private  INewsGroupRepository newsGroupRepository;
         private NewsContext db = new NewsContext();
+        public NewsController()
+        {
+            newsGroupRepository = new NewsGroupRepository(db);
+            newsRepository = new NewsRepository(db);
+        }
 
         // GET: Admin/News
         public ActionResult Index()
         {
-            var news = db.news.Include(n => n.newsGroups);
-            return View(news.ToList());
+           
+            return View(newsRepository.GetAllNewsREpository());
         }
 
         // GET: Admin/News/Details/5
@@ -28,7 +36,7 @@ namespace News_site.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            News news = db.news.Find(id);
+            News news = newsRepository.GetNewsById(id.Value);
             if (news == null)
             {
                 return HttpNotFound();
@@ -39,7 +47,7 @@ namespace News_site.Areas.Admin.Controllers
         // GET: Admin/News/Create
         public ActionResult Create()
         {
-            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle");
+            ViewBag.GroupId = new SelectList(newsGroupRepository.GetAllNewsGroups(), "GroupId", "GroupTitle");
             return View();
         }
 
@@ -54,12 +62,17 @@ namespace News_site.Areas.Admin.Controllers
             {
                 news.CreateDate = DateTime.Now;
                 news.visited = 0;
-                db.news.Add(news);
-                db.SaveChanges();
+                if(imgUp != null)
+                {
+                    news.ImageName = Guid.NewGuid() + Path.GetExtension(imgUp.FileName);
+                    imgUp.SaveAs(Server.MapPath("/NewsImages" + news.ImageName));
+                }
+                newsRepository.insertNews(news);
+                newsRepository.save();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            ViewBag.GroupId = new SelectList(newsGroupRepository.GetAllNewsGroups(), "GroupId", "GroupTitle", news.GroupId);
             return View(news);
         }
 
@@ -70,12 +83,12 @@ namespace News_site.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            News news = db.news.Find(id);
+            News news = newsRepository.GetNewsById(id.Value);
             if (news == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            ViewBag.GroupId = new SelectList(newsGroupRepository.GetAllNewsGroups(), "GroupId", "GroupTitle", news.GroupId);
             return View(news);
         }
 
@@ -88,11 +101,11 @@ namespace News_site.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Entry(news).State = EntityState.Modified;
-                db.SaveChanges();
+                newsRepository.updateNews(news);
+                newsRepository.save();
                 return RedirectToAction("Index");
             }
-            ViewBag.GroupId = new SelectList(db.newsGroups, "GroupId", "GroupTitle", news.GroupId);
+            ViewBag.GroupId = new SelectList(newsGroupRepository.GetAllNewsGroups(), "GroupId", "GroupTitle", news.GroupId);
             return View(news);
         }
 
@@ -103,7 +116,7 @@ namespace News_site.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            News news = db.news.Find(id);
+            News news = newsRepository.GetNewsById(id.Value);
             if (news == null)
             {
                 return HttpNotFound();
@@ -116,9 +129,9 @@ namespace News_site.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            News news = db.news.Find(id);
-            db.news.Remove(news);
-            db.SaveChanges();
+
+            newsRepository.DeleteNews(id);
+            newsRepository.save();
             return RedirectToAction("Index");
         }
 
@@ -127,6 +140,8 @@ namespace News_site.Areas.Admin.Controllers
             if (disposing)
             {
                 db.Dispose();
+                newsRepository.Dispose();
+                newsGroupRepository.Dispose();
             }
             base.Dispose(disposing);
         }
